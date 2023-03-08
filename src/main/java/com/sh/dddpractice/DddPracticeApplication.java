83e22759c15afcc6d989073a84367be53918f5ba
OOOIOOOIO <polite159@gmail.com>
2023-03-08 19:59:34 +0900

    first commit

---
+++ src/main/java/com/sh/dddpractice/DddPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sh.dddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/dddpractice/DddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.dddpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
